49f6f722494435aad2e60e3a0761d8fa534e47f3
Tim Sherry <tsherry82@gmail.com>
2019-08-09 20:36:42 -0400

    added functioning API keys that generate responses

--- liri.js
+++ liri.js
@@ -1,30 +1,64 @@
 require("dotenv").config();
+var axios = require('axios');
 
 var keys = require("./keys.js");
 
-// var spotify = new spotify(key.spotify);
+var spotify = new spotify(keys.spotify);
 
 var action = process.argv[2];
 var search = process.argv[3];
 
 switch (action) {
     case "concert-this":
-        console.log("Concert Test");
+        concert();
         break;
 
     case "spotify-this-song":
-        console.log("Spotify Test");
+        spotify();
         break;
 
     case "movie-this":
-        console.log("Movie Test");
+        movie();
         break;
 
     case "do-what-it-says":
-        console.log("Bossy Test");
+        bossy();
         break;
 
     default:
+        console.log("does not match any case")
         break;
 
+}
+// Find concerts using API
+
+function concert(){
+   var BandsURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
+
+   axios.get(BandsURL)
+   .then(function (response) {
+     // handle success
+     console.log(response);
+   })
+   .catch(function (error) {
+     // handle error
+     console.log(error);
+   })
+   .finally(function () {
+     // always executed
+   });
+ 
+    console.log("concert", search)
+} 
+
+function spotify(){
+    console.log("spotify", search)
+}
+
+function movie(){
+    console.log("movie", search)  
+} 
+
+function bossy(){
+    console.log("do what it says")
 }
